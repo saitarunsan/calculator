5e883a4d1cb5c1cae166fc9bc08af1641ae23490
sai tarun <saitarunsan@gmail.com>
2022-05-11 13:54:52 +0530

    few changes in testcalc

--- testcalc.py
+++ testcalc.py
@@ -2,15 +2,23 @@ import unittest
 from unittest.mock import MagicMock
 import calc
 class TestCalc(unittest.TestCase):
-    def test_clear(self):
 
+    def test_clear(self):
         self.assertEqual(calc.expression,'')
 
+    def test_notclear(self):
+        self.assertNotEqual(calc.expression,'string')
+
+
+
     def test_btn_clck(self,item='6'):
-        expression = '6'
-        #item = '6'
         calc.btn_click = MagicMock(return_value=item)
         self.assertEqual(calc.btn_click(item),'6')
+
+    def test_btn_clcknoteql(self,item='6'):
+        calc.btn_click = MagicMock(return_value=item)
+        self.assertNotEqual(calc.btn_click(item),'66')
+
 
 
     def test_btnoteql(self,expression='6+6'):

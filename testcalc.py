7535dea4863d551df2f36658f8fec1322f7a835f
sai tarun <saitarunsan@gmail.com>
2022-05-13 12:33:28 +0530

    second commit few more test cases

--- testcalc.py
+++ testcalc.py
@@ -10,25 +10,23 @@ class TestCalc(unittest.TestCase):
         self.assertNotEqual(calc.expression,'string')
 
 
-
     def test_btn_clck(self,item='6'):
         calc.btn_click = MagicMock(return_value=item)
         self.assertEqual(calc.btn_click(item),'6')
-
-    def test_btn_clcknoteql(self,item='6'):
-        calc.btn_click = MagicMock(return_value=item)
-        self.assertNotEqual(calc.btn_click(item),'66')
-
-
 
     def test_btnoteql(self,expression='6+6'):
         #expression = '6+6'
         calc.bt_equal= MagicMock(return_value = eval(expression))
         self.assertNotEqual(calc.bt_equal(expression),11)
 
+
     def test_bteql(self,expression='6+6'):
         #expression = '6+6'
         calc.bt_equal= MagicMock(return_value = eval(expression))
         self.assertEqual(calc.bt_equal(expression),12)
+        
+    def test_btn_clcknoteql(self,item='6'):
+        calc.btn_click = MagicMock(return_value=item)
+        self.assertNotEqual(calc.btn_click(item),'66')
 
 
